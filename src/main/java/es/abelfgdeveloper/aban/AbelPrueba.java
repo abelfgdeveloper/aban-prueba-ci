d45924c39e94e620678bd946b03867c4db2eb4ad
abelfgdeveloper <abelfgdeveloper@gmail.com>
2022-11-29 23:31:23 +0100

    Prueba commit

---
+++ src/main/java/es/abelfgdeveloper/aban/AbelPrueba.java
@@ -0,0 +1,22 @@
+package es.abelfgdeveloper.aban;
+
+public class AbelPrueba {
+
+    public String nombre;
+
+    public AbelPrueba() {
+
+    }
+
+    public AbelPrueba(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
